2de9c87930bac2b150e4b5fc74ad7afac59fee40
Patric Anderson <patric.a.v.s2@gmail.com>
2026-08-02 11:06:43 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os
+from telebot.async_telebot import AsyncTeleBot
 
-# Credenciais
+# Credenciais[cite: 4]
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 0))
+WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
 
-# Categorias
+# Categorias[cite: 4]
 CATEGORIAS_DISPONIVEIS = {
     "filmes": "🎬 Filmes, Séries & Animes",
     "adulto": "🔞 Adulto / NSFW",
@@ -22,5 +24,8 @@ CATEGORIAS_DISPONIVEIS = {
     "geral": "🌐 Variedades & Geral"
 }
 
-# Dicionário compartilhado em memória para criar os links fixos
+# Dicionário compartilhado em memória para criar os links fixos[cite: 4]
 admin_estados = {}
+
+# Instância global e assíncrona do bot
+bot = AsyncTeleBot(BOT_TOKEN)
